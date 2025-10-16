9d5885a909bd35e337b0dc64fefede0b6362eaaa
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-17 00:46:26 +0900

    a

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -21,7 +21,7 @@ export default async function LatestInvoices() {
                   "flex flex-row items-center justify-between py-4",
                   {
                     "border-t": i !== 0,
-                  }
+                  },
                 )}
               >
                 <div className="flex items-center">
