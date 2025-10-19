ff0a594134c1d2b373ad2ebf473072723077713e
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-19 20:04:27 +0900

    a

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -98,8 +98,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice"); // debug.
-
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
